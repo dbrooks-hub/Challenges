69aec4aba801b8f92a9a67b4d0de9ed116d30fa4
dbrooks-hub <dbrooks@doterra.com>
2019-10-04 22:36:25 -0600

    Update Using Essential Oils

--- howtouseessentialoils/usingessentialoils.py
+++ howtouseessentialoils/usingessentialoils.py
@@ -19,26 +19,15 @@ class Using_Essential_Oils(unittest.TestCase):
         driver = self.driver
         driver.get("https://www.doterra.com/US/en/using-essential-oils")
         self.assertIn("How to Use Essential Oils", driver.title)
-        #most_popular = driver.find_elements(By.XPATH, '//*[@id=\"content_body\"]//ol[1]/li')
-        #WebDriverWait(driver, timeout=10, poll_frequency=.5) \
-            #.until(expected_conditions.presence_of_element_located((By.XPATH, '//*[@id=\"content_body\"]//ol[1]/li')))
-        #print(len(most_popular))
-        #for x in most_popular:
-            #print(x.text + "-" + x.get_attribute("href"))
-
-        #self.while_loop()
-# method?
-    #def while_loop(self):
-        #driver = self.driver
-        time.sleep(5)
-        most_popular = driver.find_elements(By.XPATH, '//*[@id=\"content_body\"]//ol[1]/li')
+        most_popular = driver.find_elements(By.XPATH, '//*[@id=\"content_body\"]//ol[1]/li/a')
         WebDriverWait(driver, timeout=10, poll_frequency=.5) \
-            .until(expected_conditions.presence_of_element_located((By.XPATH, '//*[@id=\"content_body\"]//ol[1]/li')))
-        i = 0
-        while i < len(most_popular):
-            print(most_popular[i].text + "-" + most_popular[i].get_attribute("href"))
-            i += 0
-        # Assertions
+            .until(expected_conditions.presence_of_element_located((By.XPATH, '//*[@id=\"content_body\"]//ol[1]/li/a')))
+        print(len(most_popular))
+        for x in most_popular:
+            print(x.text + "-" + x.get_attribute("href"))
+
+        # Add URL link check
+        # Add Assertions
 
 if __name__ == '__main__':
     unittest.main()
