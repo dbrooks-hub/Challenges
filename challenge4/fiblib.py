8af54fb1036d08b3303e08a651c08729b61fa106
dbrooks-hub <dbrooks@doterra.com>
2019-10-08 21:27:06 -0600

    add fiblib

---
+++ challenge4/fiblib.py
@@ -0,0 +1,32 @@
+class fibonacci_sequence:
+
+    def fibR(n):
+       """Recursive function to print Fibonacci sequence"""
+       if n <= 1:
+           return n
+        else:
+            return(fibR(n-1) + fibR(n-2))
+
+# Change this value for a different result
+#nterms = 9
+
+# uncomment to take input from the user
+#nterms = int(input("How many terms? "))
+
+# check if the number of terms is valid
+#if nterms <= 0:
+   #print("Please enter a positive integer")
+#else:
+   #print("Fibonacci sequence:")
+   #for i in range(nterms):
+       #print(fibR(i))
+
+    #def fibR(n):
+    #if n==1 or n==2:
+        #return 1
+    #return fibR(n-1)+fibR(n-2)
+    #print fibR(5)
+
+class num2str:
+
+    def convert_num2words:
