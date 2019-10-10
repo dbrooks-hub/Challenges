b2d7c1e4dd0ed22a9e3240d95af06336e12566f8
dbrooks-hub <dbrooks@doterra.com>
2019-10-10 00:41:24 -0600

    WIP Challenge4

--- challenge4/challenge4_test.py
+++ challenge4/challenge4_test.py
@@ -13,6 +13,19 @@ class Challenge4(unittest.TestCase):
     def test_fibseq(self):
         seqf = [0, 1, 1, 2, 3, 5, 8, 13, 21]
         self.assertTrue(seqf == FibSeq.fib(9), FibSeq.fib(9))
+        print(seqf == FibSeq.fib(9), FibSeq.fib(9))
+
+    #def test_num2text(self):
+        #singledig = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+        #teens = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+        #tens = ["ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+        #secondval = 5
+        #leastsignval = 1
+        #print(singledig[leastsignval]) = > one
+        #print(tens[secondval]) = > fifty
+        #concantionate strings
+        # print(tens[secondval] + singledig[leastsignval]) = > fiftyone
 
 
 
