5b4209a6a6d4df19a9778a3ef014c75cde25990e
dbrooks-hub <dbrooks@doterra.com>
2019-10-15 19:09:57 -0600

    WIP Num2text

--- challenge4/fiblib.py
+++ challenge4/fiblib.py
@@ -48,7 +48,21 @@ class FibSeq:
 
 class NumSeq:
 
-    def num2txt(self):
+    @staticmethod
+    def num2txt(num):
         """function to print numbers to text"""
-        if n < 0:
+        numdict = {0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", \
+                     10: "Ten", 11: "Eleven", 12: "Twelve", 13: "Thirteen", 14: "Fourteen", 15: "Fifteen", 16: "Sixteen", \
+                     17: "Seventeen", 18: "Eighteen", 19: "Nineteen", 20: "Twenty", 30: "Thirty", 40: "Forty", 50: "Fifty", \
+                   60: "Sixty", 70: "Seventy", 80: "Eighty", 90: "Ninety"}
+        bigs = ['thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintrillion', 'sextillion', 'septillion']
+
+        if num < 0:
             raise ValueError("Must use positive integers")
+
+        #THIS IS NOT WORKING
+        if(1 <= num < 19):
+            return(numdict[num])
+        elif 20 <= num < 99:
+            tens, below_ten = divmod(num, 10)
+            return((numdict[tens - 2] + '-' + str(numdict[below_ten].lower)))
