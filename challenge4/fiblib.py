082a4eef8969a52100f26a5821e63a362ce00768
dbrooks-hub <dbrooks@doterra.com>
2019-11-02 15:22:43 -0600

    Rethinking...WIP

--- challenge4/fiblib.py
+++ challenge4/fiblib.py
@@ -62,6 +62,11 @@ class NumSeq:
         return ((NumSeq.numdict[tens * 10] + '-' + str(NumSeq.numdict[below_ten].lower())))
 
     @staticmethod
+    def tpldig(num):
+        hundreds, tens = divmod(num, 100)
+        tenssep = NumSeq.dbldig(tens.)
+
+    @staticmethod
     def num2txt(num):
         """This method uses dictionary and array to function to print numbers to text"""
         if num < 0:
@@ -73,7 +78,7 @@ class NumSeq:
         elif (20 <= num <= 99):
             return NumSeq.dbldig(num)
         #WIP.
-        # Thinking I need to turn the two digit and 3 digit into seperate functions to call for each 3 digit integer return?
+        # Thinking I need to turn the two digit and 3 digit into separate functions to call for each 3 digit integer return?
         # Below isn't working.
         # Call the two digit for less than 20 with following special cases:
         # Address: 0 in tens for [01, 02,03,04,05,06,07,08,09,]
@@ -82,10 +87,15 @@ class NumSeq:
         elif(100 <= num <= 999):
             hundreds, tens = divmod(num, 100)
             tenssep=NumSeq.dbldig(tens)
-            # if num >= 10:
-            #     sngl = divmod(num, 10).lstrip ('0')
-            #     #(sngl.lstrip('0')) ?
-            return NumSeq.numdict[hundreds] + ' hundred and ' + tenssep
+            #if (00 <= num <= 09):
+                #sngl = divmod(num, 10)
+                #return NumSeq.numdict[hundreds] + ' hundred and ' + tenssep.lstrip('0')
+            if num == 10:
+                return NumSeq.numdict[hundreds] + ' hundred' + tenssep.rstrip('0')
+            elif (11 <= num <= 19):
+                return NumSeq.numdict[hundreds] + ' hundred' + str(NumSeq.numdict[num])
+            elif (20<=num<=99):
+                return NumSeq.numdict[hundreds] + ' hundred' + tenssep
             # if 20 <= num <=99:
             #     tens, below_ten = divmod(num, 10)
             # return(((numdict[hundreds] + 'hundred ' + (numdict[tens * 10] + '-' + str(numdict[below_ten].lower)))))
