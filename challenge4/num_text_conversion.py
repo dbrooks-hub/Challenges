b2e51bc44feaa69220240ea2ed26ed8a418bcabe
dbrooks-hub <dbrooks@doterra.com>
2019-11-03 13:17:54 -0700

    WIP Num_text_conversion - added dbl dig & triple dig function

--- challenge4/num_text_conversion.py
+++ challenge4/num_text_conversion.py
@@ -1,4 +1,7 @@
 class NumSeq:
+    def __init__(self, number=0):
+        self.number = number
+
     numdict = {0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight",
                9: "Nine", \
                10: "Ten", 11: "Eleven", 12: "Twelve", 13: "Thirteen", 14: "Fourteen", 15: "Fifteen", 16: "Sixteen", \
@@ -8,47 +11,49 @@ class NumSeq:
 
     @staticmethod
     def dbldig(num):
+        dbl_return = ""
+        if num == 0:
+            return ""
         tens, below_ten = divmod(num, 10)
-        return ((NumSeq.numdict[tens * 10] + '-' + str(NumSeq.numdict[below_ten].lower())))
+        if 1 < tens:
+            dbl_return += NumSeq.numdict[tens * 10]
+            if 0 < below_ten:
+                dbl_return += '-' + NumSeq.numdict[below_ten].lower()
+        elif tens == 1:
+            dbl_return += NumSeq.numdict[10 + below_ten]
+        elif tens == 0:
+            dbl_return += NumSeq.numdict[below_ten]
+        return dbl_return
 
     @staticmethod
     def tpldig(num):
+        tpl_return = ""
+        if num == 0:
+            return ""
         hundreds, tens = divmod(num, 100)
         tenssep = NumSeq.dbldig(tens)
+        if 0 < hundreds:
+            tpl_return += NumSeq.numdict[hundreds] + ' hundred'
+            if (0 < tens):
+                tpl_return += ' and ' + tenssep
+        elif hundreds==0:
+            tpl_return += ' and ' + tenssep
+        return tpl_return
 
     @staticmethod
     def num2txt(num):
         """This method uses dictionary and array to function to print numbers to text"""
         if num < 0:
             raise ValueError("Must use positive integers")
-        if (num == 0):
+        if num == 0:
             return (NumSeq.numdict[num])
-        if(1 <= num <= 19):
+        if 1 <= num <= 19:
             return(NumSeq.numdict[num])
-        elif (20 <= num <= 99):
+        elif 20 <= num <= 99:
             return NumSeq.dbldig(num)
-        #WIP.
-        # Thinking I need to turn the two digit and 3 digit into separate functions to call for each 3 digit integer return?
-        # Below isn't working.
-        # Call the two digit for less than 20 with following special cases:
-        # Address: 0 in tens for [01, 02,03,04,05,06,07,08,09,]
-        # Address: adding the word "and " for the above as well as [10,11,12,13,14,15,16,17,18,19]
-        # Call the two digit for greater than 20
-        elif(100 <= num <= 999):
-            hundreds, tens = divmod(num, 100)
-            tenssep=NumSeq.dbldig(tens)
-            #if (00 <= num <= 09):
-                #sngl = divmod(num, 10)
-                #return NumSeq.numdict[hundreds] + ' hundred and ' + tenssep.lstrip('0')
-            if num == 10:
-                return NumSeq.numdict[hundreds] + ' hundred' + tenssep.rstrip('0')
-            elif (11 <= num <= 19):
-                return NumSeq.numdict[hundreds] + ' hundred' + str(NumSeq.numdict[num])
-            elif (20<=num<=99):
-                return NumSeq.numdict[hundreds] + ' hundred' + tenssep
-            # if 20 <= num <=99:
-            #     tens, below_ten = divmod(num, 10)
-            # return(((numdict[hundreds] + 'hundred ' + (numdict[tens * 10] + '-' + str(numdict[below_ten].lower)))))
+        elif 100 <= num <= 999:
+            return NumSeq.tpldig(num)
+
 
         #WIP - All the Bigs (pairs of 3)
 
