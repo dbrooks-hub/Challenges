f74d0f62418a548ad1a2d899a6f52eb5bc347a8b
dbrooks-hub <dbrooks@doterra.com>
2019-11-14 21:08:13 -0700

    adding/fixing unittest & assertions WIP

--- challenge4/challenge4_test.py
+++ challenge4/challenge4_test.py
@@ -20,34 +20,54 @@ class Challenge4(unittest.TestCase):
     def test_dbl_dig(self):
         """Unittest for the function dbl_dig"""
         seqt = [10, 11, 20, 22, 30, 40, 50, 60, 70, 80, 90, 99,]
-        expected = [Ten, Eleven, Twenty, Twenty-two, Thirty, Forty, Fifty, Sixty, Seventy, Eighty, Ninety, Ninety-nine]
-        for i in seqt:
-            print(NumSeq.dbl_dig(i))
-            #Todo add unittest for dbldig & add assertions
+        expected = ['Ten', 'Eleven', 'Twenty', 'Twenty-two', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy',
+                    'Eighty', 'Ninety', 'Ninety-nine']
+
+        for i in range(0, len(seqt)):
+            self.assertEqual(NumSeq.dbl_dig(seqt[i]), expected[i])
 
     def test_tpl_dig(self):
-        # TODO change to unittest for tpldig & add assertions
         """Unittest for special cases in NumSeq"""
         seqh = [100, 101, 110, 113, 125, 500, 501, 510, 513, 536, 999]
-        for i in seqh:
-            print(NumSeq.tpl_dig(i))
+        expecth = ['One hundred', 'One hundred and one', 'One hundred and ten', 'One hundred and thirteen',
+                   'One hundred and twenty-five', 'Five hundred', 'Five hundred and one', 'Five hundred and ten',
+                   'Five hundred and thirteen', 'Five hundred and thirty-six', 'Nine hundred and ninety-nine']
+
+        for i in range (0, len(seqh)):
+            self.assertEqual(NumSeq.tpl_dig(seqh[i]), expecth[i])
+            # Uncomment below to print
+            #print(NumSeq.tpl_dig(i))
 
     def test_get_tpl(self):
-        seqtpl = [1, 10, 11, 20, 22, 99, 100, 101, 109, 110, 111,
-                120, 122, 999, 1000, 1001, 1010, 1013, 1020, 1022, 1234, 1204, 1034, 12345, 12305,
-                12045, 10345, 123456, 123406, 123056, 120456, 103456, 1234567, 1234507, 1234067, 1230567, 1204567, 1034567]
-
-        sget = NumSeq.get_tpl(seqtpl)
-        stxt = NumSeq.num2txt(seqtpl)
-        assert len(sget) == len(seqtpl)
-        assert len(stxt) == len(seqtpl)
-        assert len(sget) == len(stxt)
-
+        seqtpl = [1, 10, 11, 100, 101, 110, 122, 999, 1000, 1001, 1010, 1013, 1020, 1234, 1204, 12345, 12305,
+                123456, 123406, 123056, 1234567, 1234067, 1034567]
+        expectedb = [
+            [1],
+            [10],
+            [11],
+            [100],
+            [101],
+            [110],
+            [122],
+            [999],
+            [0, 1],
+            [1, 1],
+            [10, 1],
+            [13, 1],
+            [20, 1],
+            [234, 1],
+            [204, 1],
+            [345, 12],
+            [305, 12],
+            [456, 123],
+            [406, 123],
+            [56, 123],
+            [567, 234, 1],
+            [67, 234, 1],
+            [567, 34, 1]
+        ]
         for i in range (0, len(seqtpl)):
-            print(i)
-
-            #TODO add unittest for get tpl & add assertions
-
+            self.assertEqual(NumSeq.get_tpl(seqtpl[i]), expectedb[i])
 
     def test_num2text(self):
         """ Unittest for NumSeq.num2txt"""
