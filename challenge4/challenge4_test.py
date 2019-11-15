c08559fa74f41f37a869855faf35ce0f0030adfc
dbrooks-hub <dbrooks@doterra.com>
2019-11-14 19:43:46 -0700

    adding for unittests

--- challenge4/challenge4_test.py
+++ challenge4/challenge4_test.py
@@ -20,6 +20,7 @@ class Challenge4(unittest.TestCase):
     def test_dbl_dig(self):
         """Unittest for the function dbl_dig"""
         seqt = [10, 11, 20, 22, 30, 40, 50, 60, 70, 80, 90, 99,]
+        expected = [Ten, Eleven, Twenty, Twenty-two, Thirty, Forty, Fifty, Sixty, Seventy, Eighty, Ninety, Ninety-nine]
         for i in seqt:
             print(NumSeq.dbl_dig(i))
             #Todo add unittest for dbldig & add assertions
@@ -36,17 +37,28 @@ class Challenge4(unittest.TestCase):
                 120, 122, 999, 1000, 1001, 1010, 1013, 1020, 1022, 1234, 1204, 1034, 12345, 12305,
                 12045, 10345, 123456, 123406, 123056, 120456, 103456, 1234567, 1234507, 1234067, 1230567, 1204567, 1034567]
 
+        sget = NumSeq.get_tpl(seqtpl)
+        stxt = NumSeq.num2txt(seqtpl)
+        assert len(sget) == len(seqtpl)
+        assert len(stxt) == len(seqtpl)
+        assert len(sget) == len(stxt)
+
+        for i in range (0, len(seqtpl)):
+            print(i)
+
             #TODO add unittest for get tpl & add assertions
 
 
     def test_num2text(self):
         """ Unittest for NumSeq.num2txt"""
-        dig = FibSeq.fib(14)
+        dig = FibSeq.fib(30)
         for i in dig:
             # TODO assertions
             print(NumSeq.num2txt(i))
 
-    #def test_get_fib_seq_with_text(self):
+    def test_get_fib_seq_with_text(self):
+
+        print(FibSeqFactory)
         #TODO add FibFactory test & assertions
 
 if __name__ == '__main__':
